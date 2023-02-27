79c2c0e9e5eb3fbad8c081f909fc76e15bfc5c35
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-27 08:23:38 -0300

    Configurando o endpoint de introspecção de tokens

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -26,7 +27,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret(passwordEncoder.encode("web123"))
                 .authorizedGrantTypes("password")
                 .scopes("write", "read")
-                .accessTokenValiditySeconds(60 * 60 * 6);
+                .accessTokenValiditySeconds(60 * 60 * 6); //6 horas (padrão é 12 horas)
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //security.checkTokenAccess("isAuthenticated()"); //Basic Authentication
+        security.checkTokenAccess("permitAll()"); //No Auth
     }
 
     @Override
